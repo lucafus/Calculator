06fdf7994a75b255570145209871f875a1749029
lucafus <luca.fusaro1996@gmail.com>
2024-01-09 16:28:29 -0300

    calculus logic completed, error handling in progress

--- logic.js
+++ logic.js
@@ -1,80 +1,97 @@
 
 
-const calculator = document.querySelector(".calculator")
-const keys = calculator.querySelector(".buttons")
-const display = document.querySelector(".display")
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".buttons");
+const display = calculator.querySelector(".display");
+
+
+
+const calculate =(n1,operator, n2) => { 
+
+    let result = ''
+
+    if (operator === 'add') {
+        result = parseFloat(n1) + parseFloat(n2)
+    }
+
+    else if (operator === 'subtract') {
+
+        result = parseFloat(n1) - parseFloat(n2)
+    }
+
+    else if (operator === 'divide') {
+
+        result = parseFloat(n1) / parseFloat(n2) 
+    }
+
+    else if (operator === 'multiply') {
+
+        result = parseFloat(n1) * parseFloat(n2)
+    }
+
+    return result
+}
+
 
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
 
-        const key = e.target
-        const action = key.dataset.action
-        const keyContent = key.textContent 
-        const displayedNum = display.textContent
 
-        if (action === 'decimal') { display.textContent = displayedNum + '.'} 
+        Array.from(key.parentNode.children)
+        .forEach(k => k.classList.remove('is-depressed'));
 
-        if (!action) {if (displayedNum === '0')  {display.textContent = keyContent}
         
-        else {display.textContent = displayedNum + keyContent}
-
-        if (action === 'add' ||
-        action === 'divide' ||
-        action === 'multiply' ||
-        action === 'substract'  
-         ) {
-        key.classList.add('is-depressed')
-
-        const previousKeyType = calculator.dataset.previousKeyType
-
         if (!action) {
             if (displayedNum === '0' || previousKeyType === 'operator') {
-                display.textContent = keyContent}
-
-                else {display.textContent = displayedNum + keyContent}
+                display.textContent = keyContent;
+            } else {
+                display.textContent +=  keyContent;
             }
-        
-        calculator.dataset.previousKeyType = 'operator'
-
         }
-    }
-    }
 
 
-})
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.';
+        }
+
+        if (action === 'clear') {
+            console.log('clear key!');
+        }
+
+        if (action === 'add' ||
+            action === 'divide' ||
+            action === 'multiply' ||
+            action === 'subtract') {
+            key.classList.add('is-depressed');
+
+                
+            calculator.dataset.firstValue = displayedNum
+            calculator.dataset.operator = action
+            calculator.dataset.previousKeyType = 'operator';
+            }
+
+            if (action === 'calculate') {
+                const firstValue = calculator.dataset.firstValue
+                const operator = calculator.dataset.operator
+                const secondValue = displayedNum;
+
+                display.textContent = calculate(firstValue, operator, secondValue)
+                
+            }
 
 
-keys.addEventListener("click", e => {
- if (e.target.matches("button")) {
-   
-const key = e.target
-const action = key.dataset.action
+            
+        }
 
+     
 
+});
 
-if (action === 'add' ||
-action === 'divide' ||
-action === 'multiply' ||
-action === 'substract'  
- ) {
-    console.log('operator key!')
-  } 
-  
-
-if (!action) {
-    console.log('number key!')
-  }
-
-
-if (action === 'clear') { console.log('clear key!')}
-
-if (action === 'calculate')  {console.log('calculate key!')}
-
-if (action === 'decimal') {console.log('decimal key!')}
-
-
- } 
-})
 
 
