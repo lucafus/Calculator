91fa5c80a7c864c45f058ecb97490828ef442dbd
lucafus <luca.fusaro1996@gmail.com>
2024-01-10 15:53:19 -0300

    fault proof logic in progress, clear button in process of creation

--- logic.js
+++ logic.js
@@ -42,56 +42,104 @@ keys.addEventListener('click', e => {
         const displayedNum = display.textContent;
         const previousKeyType = calculator.dataset.previousKeyType;
 
+        
 
         Array.from(key.parentNode.children)
         .forEach(k => k.classList.remove('is-depressed'));
 
         
         if (!action) {
-            if (displayedNum === '0' || previousKeyType === 'operator') {
+            if (
+                displayedNum === '0' || 
+                previousKeyType === 'operator' ||
+                previousKeyType === 'calculate'
+            
+            ) {
                 display.textContent = keyContent;
             } else {
-                display.textContent +=  keyContent;
+                display.textContent =  displayedNum + keyContent;
             }
+
+            calculator.dataset.previousKeyType = 'number'
         }
 
 
         if (action === 'decimal') {
-            display.textContent = displayedNum + '.';
-        }
+            
+           if (!displayedNum.includes('.')) {
+           display.textContent = displayedNum + '.'
+        } else if (
+            
+            previousKeyType === 'operator' ||
+            previousKeyType === 'calculate') {
+                
+                display.textContent = '0.'
+            }
 
+        calculator.dataset.previousKeyType = 'decimal'
+
+    }
+       
         if (action === 'clear') {
             console.log('clear key!');
+
+            calculator.dataset.previousKeyType = 'clear'
         }
 
         if (action === 'add' ||
             action === 'divide' ||
             action === 'multiply' ||
             action === 'subtract') {
+
+        const firstValue = calculator.dataset.firstValue
+        const operator = calculator.dataset.operator
+        const secondValue = displayedNum
+
+if (firstValue && 
+    operator &&
+    previousKeyType !== 'operator' &&
+    previousKeyType !== 'calculate'
+    ) {
+        
+        const calcValue = calculate(firstValue, operator, secondValue)
+        display.textContent = calcValue 
+        calculator.dataset.firstValue = calcValue } else {
+            calculator.dataset.firstValue = displayedNum
+}
+
+
             key.classList.add('is-depressed');
 
-                
-            calculator.dataset.firstValue = displayedNum
             calculator.dataset.operator = action
             calculator.dataset.previousKeyType = 'operator';
             }
 
             if (action === 'calculate') {
-                const firstValue = calculator.dataset.firstValue
+                let firstValue = calculator.dataset.firstValue
                 const operator = calculator.dataset.operator
-                const secondValue = displayedNum;
+                let secondValue = displayedNum;
+
+            if (firstValue) {
+
+                 if (previousKeyType === 'calculate') {
+                    
+                    secondValue = calculator.dataset.modValue 
+                    firstValue = displayedNum
+                 }
 
                 display.textContent = calculate(firstValue, operator, secondValue)
                 
             }
 
-
-            
+            calculator.dataset.modValue = secondValue
+            calculator.dataset.previousKeyType = 'calculate'
+  
         }
+    
+    }
 
-     
 
 });
 
 
-
+ 
